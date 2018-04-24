23dc40966d4170743df4865ba2e86b4e0d8fa162
Dmytro Danylyk <dmytrodanylyk@gmail.com>
2018-04-24 22:37:27 +1000

    Fixed test

--- app/src/test/java/com/dmytrodanylyk/MainPresenterTest.kt
+++ app/src/test/java/com/dmytrodanylyk/MainPresenterTest.kt
@@ -1,11 +1,14 @@
 package com.dmytrodanylyk
 
-import com.dmytrodanylyk.cancel.DataProviderAPI
-import com.dmytrodanylyk.cancel.MainPresenter
-import com.dmytrodanylyk.cancel.MainView
+import kotlinx.coroutines.experimental.CommonPool
+import kotlinx.coroutines.experimental.Unconfined
+import kotlinx.coroutines.experimental.android.UI
+import kotlinx.coroutines.experimental.async
+import kotlinx.coroutines.experimental.launch
 import org.junit.Test
 import org.mockito.ArgumentMatchers
 import org.mockito.Mockito.*
+import kotlin.coroutines.experimental.CoroutineContext
 import kotlin.coroutines.experimental.EmptyCoroutineContext
 import org.mockito.Mockito.`when` as whenMockito
 
@@ -20,7 +23,7 @@ class MainPresenterTest {
         val mockDataProvider = mock(DataProviderAPI::class.java)
         whenMockito(mockDataProvider.loadData(ArgumentMatchers.anyString())).thenReturn(MOCK_RESULT)
 
-        val presenter = MainPresenter(mockView, mockDataProvider, EmptyCoroutineContext, EmptyCoroutineContext)
+        val presenter = MainPresenter(mockView, mockDataProvider, Unconfined, Unconfined)
 
         // test
         presenter.startPresenting()
@@ -31,3 +34,37 @@ class MainPresenterTest {
         verify(mockView).showData(MOCK_RESULT)
     }
 }
+
+class MainPresenter(private val view: MainView,
+                    private val dataProvider: DataProviderAPI,
+                    private val uiContext: CoroutineContext = UI,
+                    private val bgContext: CoroutineContext = CommonPool) {
+
+    fun startPresenting() {
+        loadData()
+    }
+
+    private fun loadData() = launch(uiContext) {
+        // use the provided uiContext (UI)
+        view.showLoading()
+
+        // use the provided ioContext (CommonPool)
+        val task = async(bgContext) { dataProvider.loadData("Task") }
+        val result = task.await()
+
+        view.showData(result)
+    }
+
+}
+
+interface MainView {
+    fun showLoading()
+
+    fun showData(data: String)
+}
+
+interface DataProviderAPI {
+    fun loadData(data: String): String
+}
+
+
